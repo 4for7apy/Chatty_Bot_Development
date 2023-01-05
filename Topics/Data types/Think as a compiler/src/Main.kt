3bf5689fbb931e4f8736ab23865ac1c86e8409ec
arpit <arpit88407@gmail.com>
2023-01-06 01:02:55 +0530

    Initial commit

---
+++ Topics/Data types/Think as a compiler/src/Main.kt
@@ -0,0 +1,8 @@
+fun main() {
+
+    // put your code above
+    val first:String = "Arpit"
+    val last: String = "Srivastav"
+    val age: Int = 18
+    println("My name is $first $last and I’m $age years old")
+}
---
+++ Topics/Standard output/Theory/src/Main.kt
@@ -0,0 +1,8 @@
+// You can experiment here, it won’t be checked
+
+fun main(args: Array<String>) {
+    println("I")
+    println("know")
+    println("Kotlin")
+    println("well.")
+}
